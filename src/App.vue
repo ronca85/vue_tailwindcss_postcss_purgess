3b0de491025499080ba72c49349840a54f9df349
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2019-12-06 18:48:30 +0100

    vue starter, uses tailwindcss and purgecss

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="bg-green-300 text-blue-700">Classes added to this element are only used for testing and setting up this starter. Resulted in rewriting the "serve" parameter and using concurrently.</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
+import '../public/build/main.css' // use compiled file here. when running npm run build all the unused css will be purged to keep filesize low.
 
 export default {
   name: 'app',
