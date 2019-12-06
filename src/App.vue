cb93a9f286f121664e1b59168a2dcb506d953339
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2019-12-06 19:20:30 +0100

    serialize-javascript vulnerability update

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p class="bg-green-300 text-blue-700">Classes added to this element are only used for testing and setting up this starter. Resulted in rewriting the "serve" parameter and using concurrently.</p>
+    <h1 class="text-6xl font-bold bg-green-300 text-blue-700">Classes added to this element are only used for testing and setting up this starter. Resulted in rewriting the "serve" parameter and using concurrently.</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-import '../public/build/main.css' // use compiled file here. when running npm run build all the unused css will be purged to keep filesize low.
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/css/main.css"></style>
+
